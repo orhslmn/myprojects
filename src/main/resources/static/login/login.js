bc4d49e434c4cc03c4d00c755a0b4a403a5d0210
orhslmn <orhan.suleyman@yahoo.com>
2023-11-22 15:02:32 -0500

    Ana sayfa Eklendi

--- src/main/resources/static/login/login.js
+++ src/main/resources/static/login/login.js
@@ -2,3 +2,15 @@
 Originally found at https://cssdeck.com/labs/login-form-using-html5-and-css3
 */
 
+function giris(){
+
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+
+if (username === "admin" && password === "1" ){
+ window.location.href = '/main/index.html';
+} else {
+ window.location.href = '/login/login.html';
+}
+
+}
